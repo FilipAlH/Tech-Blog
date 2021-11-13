f61b3425f1a6d5abf599b8ae9a00c98b64d35903
Filip <f2alhama@uwaterloo.ca>
2021-11-12 20:15:41 -0500

    added homepage display for all users and dashboard display for the user who is signed in to show their threads

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,12 +1,40 @@
 const router = require('express').Router();
 const withAuth = require('../utils/auth');
+const { User, Threads, Replies } = require('../models');
 
 router.get('/', async (req, res) => {
-  res.render('homepage', {loggedIn: req.session.logged_in});
+  try {
+    const getThreads = await Threads.findAll()
+      
+    const threads = getThreads.map((thread) => thread.get({plain: true}))
+    console.log(threads)
+
+    res.render('homepage', {loggedIn: req.session.logged_in, threads});
+
+  } catch (error){
+    res.json(error)
+  }
 });
 
 router.get('/dashboard', withAuth, async (req, res) => {
-  res.render('dashboard', {loggedIn: req.session.logged_in});
+  try {
+    const getThreads = await Threads.findAll(
+      {
+        where: {
+          user_id: req.session.user_id
+        }
+      }
+    )
+    const threads = getThreads.map((thread) => thread.get({plain: true}))
+    console.log(threads)
+
+    res.render('dashboard', {loggedIn: req.session.logged_in, threads});
+    
+  } catch (error){
+    res.json(error)
+  }
+
+  
 });
 
 router.get('/login', async (req, res) => {
